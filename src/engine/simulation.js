9b849d842349b5de093dbe586f917163666e021e
acehuall <mr.ed.allan@gmail.com>
2026-07-16 16:41:12 +0100

    Simplify timeline month markers

--- src/engine/simulation.js
+++ src/engine/simulation.js
@@ -74,7 +74,6 @@ function render(){
 
   var progress=(simT/QLEN*100);
   $('pfill').style.width=progress+'%';
-  $('pbar').style.setProperty('--scrub-x',progress+'%');
   $('pscrub').value=simT;
   $('ptime').textContent=simMonthLabel(simT);
   var defs=getMetricDefs();
